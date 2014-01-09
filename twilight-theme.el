d1eccf1e26b8a86a3587144832b067ccf5f564f4
xujinlai <xujinlai@gmail.com>
2014-01-09 22:16:59 +0800

    theme

---
+++ twilight-theme.el
@@ -0,0 +1,29 @@
+(deftheme twilight
+  "Created 2014-01-09.")
+
+(custom-theme-set-faces
+ 'twilight
+ '(cursor ((t (:foreground "#a7a7a7"))))
+ '(font-lock-builtin-face ((t (:foreground "#CACACA"))))
+ '(font-lock-comment-face ((t (:foreground "#5F5A60"))))
+ '(font-lock-constant-face ((t (:foreground "#CF6A4C"))))
+ '(font-lock-function-name-face ((t (:foreground "#9B703F"))))
+ '(font-lock-keyword-face ((t (:foreground "#CDA869"))))
+ '(font-lock-preprocessor-face ((t (:foreground "Aquamarine"))))
+ '(font-lock-regexp-grouping-backslash ((t (:foreground "#E9C062"))))
+ '(font-lock-regexp-grouping-construct ((t (:foreground "red"))))
+ '(minibuffer-prompt ((t (:foreground "#5F5A60"))))
+ '(ido-subdir ((t (:foreground "#CF6A4C"))))
+ '(ido-first-match ((t (:foreground "#8F9D6A"))))
+ '(ido-only-match ((t (:foreground "#8F9D6A"))))
+ '(font-lock-string-face ((t (:foreground "#8F9D6A"))))
+ '(font-lock-type-face ((t (:foreground "#9B703F"))))
+ '(font-lock-variable-name-face ((t (:foreground "#7587A6"))))
+ '(font-lock-warning-face ((t (:background "#EE799F" :foreground "red"))))
+ '(region ((t (:background "#27292A"))))
+ '(mode-line ((t (:background "grey75" :foreground "black"))))
+ '(highlight ((t (:background "#111111"))))
+ '(underline ((nil (:underline nil))))
+ '(default ((t (:background "#141414" :foreground "#f8f8f8")))))
+
+(provide-theme 'twilight)
